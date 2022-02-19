6705f89cdf766b7a0f7a55c53116202552e00454
Irina <livefortoday@mail.ru>
2022-02-19 14:47:00 +0300

    blank lines deleted

---
+++ lesson-4-closures/task-1.js
@@ -0,0 +1,32 @@
+// Задание 1.
+// Напишите программу, создающую строку, содержащую решётку 8х8, в
+// которой линии разделяются символами новой строки. На каждой позиции
+// либо пробел, либо \#.
+
+// var symbolInLine = 8;
+// var lines = 8;
+var symbolInLine = +prompt("Сколько символов должно быть в строке?", "8");
+var lines = +prompt ("Сколько линий должно быть?", "8");
+
+var symbol = "# ";
+var spaceLine = " #";
+var line = '';
+for (var i = 0; i < symbolInLine; i++) {
+  for (var j = 0; j < lines; j++) {
+    if (i % 2 == 0) {
+      line += symbol;
+    } else {
+      line += spaceLine;
+    }
+  }
+  line += '\n';
+}
+console.log(line);
+
+// Прямоугольник:
+// var symbolInLine = 8;
+// var lines = 8;
+// var symbol = "# ";
+// var line = symbol.repeat(symbolInLine) + '\n';
+// var result = line.repeat(lines);
+// console.log(result);
